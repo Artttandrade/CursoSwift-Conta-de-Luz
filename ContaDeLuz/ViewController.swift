8da6a7368295ef488aa1175a72aa3bca0160c74e
Artur Andrade <adsa@icomp.ufam.edu.br>
2022-04-07 03:31:44 -0300

    First Commit

--- ContaDeLuz/ViewController.swift
+++ ContaDeLuz/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var lastValueTextField: UITextField?
+    @IBOutlet var currentValueTextField: UITextField?
+    @IBOutlet var valueDiffLabel: UILabel?
+    let diffLabelText: String = "O valor da conta é: "
+    let taxValue: Double = 1.8
+    
+    @IBAction func calcular (){
+        
+        guard let lastValue = lastValueTextField?.text else {
+            return
+        }
+        
+        guard let currentValue = currentValueTextField?.text else {
+            return
+        }
+        
+        let lastValueInt: Int = Int(lastValue) ?? 0
+        let currentValueInt: Int = Int(currentValue) ?? 0
+        
+        let diffValue: Int = currentValueInt - lastValueInt;
+        
+        let billValue: Double = Double(diffValue) * taxValue;
+        
+        print("Calcular:-- \(billValue)")
+        
+        valueDiffLabel?.text = "\(diffLabelText) \(String(billValue))"
+        
     }
 
 
